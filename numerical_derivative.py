f2ef5f759d58ab9351cabc091fa89dc72d13d21e
Aswin Ramesh <aswinramesh527@gmail.com>
2026-03-22 20:45:42 +0530

    Implement numerical derivative

---
+++ numerical_derivative.py
@@ -0,0 +1,18 @@
+def numerical_derivative(f,x,h=0.0001):
+    return (f(x+h)-f(x))/h
+
+def f(x):
+    return x**2
+
+
+#Central Difference
+
+def central_diffrence(f,x,h=0.0001):
+    return (f(x+h)-f(x-h))/2*h
+
+x = 3
+
+print(numerical_derivative(f,x))
+
+print("Central Difference Method:",central_diffrence(f,x))
+
