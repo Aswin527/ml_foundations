b1e9e2c67216f792637efafbbf00b2bd9c060127
Aswin Ramesh <aswinramesh527@gmail.com>
2026-03-24 22:57:48 +0530

    Plot loss curve

--- regression/linear_regression.py
+++ regression/linear_regression.py
@@ -1,32 +1,43 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-X=np.array([1,2,3,4])
+X = np.array([1,2,3,4])
+y = np.array([2,4,6,8])
 
-y=np.array([2,4,6,8])
-
-w=0
-b=0
+w = 0
+b = 0
 
 learning_rate = 0.01
 epochs = 1000
 
 n = len(X)
 
+losses = []   # store loss values
+
 for i in range(epochs):
+
     y_pred = w * X + b
-    dw = (-2/n) * np.sum(X*(y-y_pred))
-    db = (-2/n) * np.sum(y-y_pred)
+    
+    # Mean Squared Error loss
+    loss = np.mean((y - y_pred) ** 2)
+    losses.append(loss)
+
+    dw = (-2/n) * np.sum(X * (y - y_pred))
+    db = (-2/n) * np.sum(y - y_pred)
 
     w = w - learning_rate * dw
     b = b - learning_rate * db
 
-print("Weight:",w)
-print("Bias:",b)
+print("Weight:", w)
+print("Bias:", b)
 
 x_new = 5
-
 prediction = w * x_new + b
+print("Prediction:", prediction)
 
-print("Prediction:",prediction)
-
-
+# Plot loss curve
+plt.plot(losses)
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.title("Loss Curve")
+plt.show()
